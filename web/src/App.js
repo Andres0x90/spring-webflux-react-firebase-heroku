1b6fba7d74ce786ca2c4ea61ed415e7d27c5eff7
Andres Cano <andrescaza246@hotmail.com>
2021-10-15 11:55:11 -0500

    Cambio en properties para despliegue

--- web/src/App.js
+++ web/src/App.js
@@ -20,12 +20,12 @@ import OwnerQuestionsPage from './pages/OwnerQuestionsPage'
 import { useAuthState } from "react-firebase-hooks/auth";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCTySyvuIDPg7RWF6ceuuwC2t3BEiAK38o",
-  authDomain: "question-app-demo.firebaseapp.com",
-  projectId: "question-app-demo",
-  storageBucket: "question-app-demo.appspot.com",
-  messagingSenderId: "1038673531562",
-  appId: "1:1038673531562:web:da90421f639a3115dcf6d3"
+  apiKey: "AIzaSyAoFn-ix8eTFTlbTfJX6zLmDbA_deQ9Li0",
+  authDomain: "question-and-answer-df3c7.firebaseapp.com",
+  projectId: "question-and-answer-df3c7",
+  storageBucket: "question-and-answer-df3c7.appspot.com",
+  messagingSenderId: "816410372904",
+  appId: "1:816410372904:web:5c35e74b668b53491f7e1d"
 });
 
 const auth = firebase.auth();
