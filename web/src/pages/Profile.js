20b8b4c60a9d5eaba5eab1ce859897853cfefc99
Andres Cano <andrescaza246@hotmail.com>
2021-10-22 13:59:06 -0500

    Se termino la funcionalidad actualizar datos del usuario

--- web/src/pages/Profile.js
+++ web/src/pages/Profile.js
@@ -1,26 +1,48 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useForm } from 'react-hook-form';
 import { connect } from 'react-redux';
 import { authFailure } from '../actions/authActions';
-import { success } from '../actions/questionActions';
+import { failure, loading, success } from '../actions/questionActions';
 import { db } from '../components/FirebaseService';
 
 
-const Profile = ({dispatch, userId, email, error}) => {
+const Profile = ({dispatch, userId, email, error, isLoading}) => {
 
     const {register, handleSubmit, setValue} = useForm();
     const userRef = db.collection("user");
     const [updateSuccess, setUpdateSucess] = useState();
 
+    useEffect(()=>
+    {
+        dispatch(loading())
+        userRef.doc(userId).get().then((doc)=>
+        {
+            if (doc.exists)
+            {
+                const data = doc.data();
+                setValue("name", data.name);
+                setValue("last_name", data.last_name);
+            }
+            dispatch(success());
+        });
+    },[]);
+
     const onSubmit = (data) =>
     {
+        dispatch(loading());
         userRef.doc(userId).set({
            "name": data.name,
             "last_name": data.last_name
         })
-        .then(()=> setUpdateSucess('Data updated successfully'))
-        .catch(() => dispatch(authFailure("Error! the information couldn't be updated")))
+        .then(()=> {
+            setUpdateSucess('Data updated successfully');
+            dispatch(success());
+        })
+        .catch(() => {
+            dispatch(authFailure("Error! the information couldn't be updated"));
+            dispatch(failure());
+        })
     }
    
     return <section>
@@ -28,11 +50,13 @@ const Profile = ({dispatch, userId, email, error}) => {
         <form className="my-3" onSubmit={handleSubmit(onSubmit)}>
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" {...register("name", {required:true})} />
+            <input type="text" class="form-control" {...register("name", {required:true})} 
+            disabled={isLoading}/>
         </div>
         <div class="mb-3">
             <label for="last_name" class="form-label">Last Name</label>
-            <input type="text" class="form-control" {...register("last_name", {required:true})} />
+            <input type="text" class="form-control" {...register("last_name", {required:true})} 
+            disabled={isLoading}/>
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
@@ -41,7 +65,7 @@ const Profile = ({dispatch, userId, email, error}) => {
              value={email}  readOnly={true}/>
         </div>
         <p>{error?error: updateSuccess}</p>
-        <button type="submit" class="btn btn-danger">Update</button>
+        <button type="submit" class="btn btn-danger">{isLoading?'Loading...':'Update'}</button>
         </form>
     </section>
 }
@@ -50,7 +74,8 @@ const mapToProps = state =>
 ({
     userId: state.auth.uid,
     email: state.auth.email,
-    error: state.auth.error
+    error: state.auth.error,
+    isLoading: state.question.loading
 })
 
 export default connect(mapToProps)(Profile);
